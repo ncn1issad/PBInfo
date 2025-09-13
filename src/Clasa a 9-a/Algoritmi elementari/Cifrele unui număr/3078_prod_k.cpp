ea1a926d566c0b958e4c7160d78db89fcbacd2a8
ncn1 <help2921@gmail.com>
2025-09-13 08:32:31 +0000

    3078

---
+++ src/Clasa a 9-a/Algoritmi elementari/Cifrele unui număr/3078_prod_k.cpp
@@ -0,0 +1,23 @@
+// 100 points, 0s, C++11
+
+#include <iostream>
+
+long long n, p = 1;
+
+bool isValid = false; // To handle the case when all digits are k
+
+short k;
+
+int main() {
+    std::cin >> n >> k;
+    while (n) {
+        int c = n % 10;
+        if (c != k) {
+            p *= c; 
+            isValid = true;
+        }
+        n /= 10;
+    }
+    if (!isValid) p = 0;
+    std::cout << p << '\n';
+}
